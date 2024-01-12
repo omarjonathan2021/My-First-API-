ffe3da3a73cce209d6ff1e400c033354d20e62bd
Omar Jonathan <omarjonathan2017@gmail.com>
2024-01-12 20:53:12 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import UserList from './UserList';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <UserList />
-      </header>
-    </div>
-  );
-}
+import IntroSection from './components/Intro'
+import ContactSection from './components/ContactSection.js'
+import MapSection from './components/Map' // import the map here
+import DisclaimerSection from './components/Disclaimer'
+import FooterSection from './components/Footer'
+
+import './App.css'
+
+const location = {
+  address: '1600 Amphitheatre Parkway, Mountain View, california.',
+  lat: 37.42216,
+  lng: -122.08427,
+} // our location object from earlier
+
+const App = () => (
+  <div className="App">
+    <IntroSection />
+    <ContactSection />
+    <MapSection location={location} zoomLevel={17} /> {/* include it here */}
+    <DisclaimerSection />
+    <FooterSection />
+  </div>
+)
 
 export default App;
